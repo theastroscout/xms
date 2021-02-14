21517b6afd7fec387d3d0f42d47af4bf9818512e
Lucky Pingwin <luckypingwin@iMac-Lucky.local>
2021-02-15 01:28:36 +0300

    Added escapeHTML method

--- sandbox/app/utils.js
+++ sandbox/app/utils.js
@@ -91,6 +91,14 @@ var utils = {
 			str += dots;
 		}
 		return str;
+	},
+	escapeHtml: (str) => {
+		return str
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
 	}
 };
 module.exports = utils;
