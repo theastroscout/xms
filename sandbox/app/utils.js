8c4560e7f46fb73cbc9c53c219ae34ccd6da2db3
Alexander Yermolenko <i@surfy.one>
2021-02-20 14:55:14 +0300

    Added utils.plural method

--- sandbox/app/utils.js
+++ sandbox/app/utils.js
@@ -130,6 +130,9 @@ var utils = {
 		.replace(/>/g, "&gt;")
 		.replace(/"/g, "&quot;")
 		.replace(/'/g, "&#039;");
+	},
+	plural: (n, forms) => {
+		return n%10==1&&n%100!=11?forms[0]:(n%10>=2&&n%10<=4&&(n%100<10||n%100>=20)?forms[1]:forms[2]);
 	}
 };
 module.exports = utils;
