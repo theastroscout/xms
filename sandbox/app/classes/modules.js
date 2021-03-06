fde21d2d6bf66e8a67617fb88bdd077c97ec3d37
Pumba <pumba@MacBook-Air-Pumba.local>
2021-03-06 23:48:10 +0300

    Added Master Call

--- sandbox/app/classes/modules.js
+++ sandbox/app/classes/modules.js
@@ -95,6 +95,14 @@ var modules = {
 			return await modules.list.view.getNotFound(url,cookies);
 		}
 		return false;
+	},
+	masterCall: async (payload) => {
+		let moduleName = payload.module;
+		let methodName = payload.method;
+		if(modules.list[moduleName] !== undefined && typeof modules.list[moduleName][methodName] === "function"){
+			return await modules.list[moduleName][methodName](data);
+		}
+		return false;
 	}
 };
 module.exports = modules;
